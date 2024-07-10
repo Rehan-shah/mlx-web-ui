ece938721d3737a7130b3f5a0d9df925a4816a26
Rehan-shah <Rehansnehalshah1@gmail.com>
2024-07-10 19:52:46 +0530

    fix: Update the update.sh so it would update mlx

--- update.sh
+++ update.sh
@@ -1,40 +1,31 @@
 
 #!/bin/bash
 
-# Function to fetch updates and check if there are new commits
-check_git_fetch() {
-    #
-    conda init 
-    conda activate mlx-backend
-    # Fetch updates from the remote repository
-
-    pip install mlx --upgrade
-    pip install mlx_lm --upgrade
-
-    echo "Upadted mlx and mlx_lm"
-
-    git fetch
-
-    # Get the name of the current branch
-    current_branch=$(git rev-parse --abbrev-ref HEAD)
-
-    # Get the commit hashes of the local and remote branches
-    local_commit=$(git rev-parse $current_branch)
-    remote_commit=$(git rev-parse origin/$current_branch)
-
-    # Compare the commit hashes
-    if [ "$local_commit" == "$remote_commit" ]; then
-        echo "The repository is already up to date."
-    else
-        cd ./backend
-        conda env update --file local.yml --prune
-        cd ../frontend
-        bun i
-
-        echo "The repository has been updated."
-    fi
-
-}
 
 
-check_git_fetch
+eval "$(conda shell.bash hook)"
+conda activate mlx-backend
+
+pip install mlx --upgrade
+pip install mlx_lm --upgrade
+
+echo "Updated mlx and mlx_lm"
+
+git fetch
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+local_commit=$(git rev-parse $current_branch)
+remote_commit=$(git rev-parse origin/$current_branch)
+
+if [ "$local_commit" == "$remote_commit" ]; then
+    echo "The repository is already up to date."
+else
+    cd ./backend
+    conda env update --file local.yml --prune
+    cd ../frontend
+    bun i
+
+    echo "The repository has been updated."
+fi
+
